0f7a809369b9751550cfcf799b314096c3d63c2a
Lucas Leonel <lucks9616@gmail.com>
2022-06-06 19:40:58 -0300

    Imagens dos planetas

--- app/src/main/java/br/com/etecia/myplanets/MainActivity.java
+++ app/src/main/java/br/com/etecia/myplanets/MainActivity.java
@@ -11,18 +11,25 @@ import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
     //Declarar a lista
     ListView listplaneta;
+    //Criando os vetores com os dados para a lista
+    String nomePlanetas[] = {"Jupiter", "Lua", "Marte", "Mercurio", "Netuno", "Plutão", "Saturno", "Sol", "Terra", "Urano", "Venus"};
+    int imagemPlanetas[] = {R.drawable.jupiter, R.drawable.lua,
+            R.drawable.marte, R.drawable.mercurio, R.drawable.netuno,
+            R.drawable.plutao,R.drawable.saturno, R.drawable.sol,
+            R.drawable.terra, R.drawable.urano, R.drawable.venus};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listplaneta = findViewById(R.id.idListaPlanetas);
-
-        listplaneta.setAdapter();
+        MyAdapter adapter = new MyAdapter();
+        listplaneta.setAdapter(adapter);
 
     }
+
     //Criando a classe interna - inner class
-    public class MyAdapter extends BaseAdapter{
+    public class MyAdapter extends BaseAdapter {
 
 
         @Override
