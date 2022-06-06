78f0f3d2a09bfaec73e6c5f7ba9bd9308e92c0e0
Lucas Leonel <lucks9616@gmail.com>
2022-06-06 20:11:28 -0300

    Implementado a lista de planetas

--- app/src/main/java/br/com/etecia/myplanets/MainActivity.java
+++ app/src/main/java/br/com/etecia/myplanets/MainActivity.java
@@ -1,12 +1,15 @@
 package br.com.etecia.myplanets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //Declarar a lista
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     String nomePlanetas[] = {"Jupiter", "Lua", "Marte", "Mercurio", "Netuno", "Plutão", "Saturno", "Sol", "Terra", "Urano", "Venus"};
     int imagemPlanetas[] = {R.drawable.jupiter, R.drawable.lua,
             R.drawable.marte, R.drawable.mercurio, R.drawable.netuno,
-            R.drawable.plutao,R.drawable.saturno, R.drawable.sol,
+            R.drawable.plutao, R.drawable.saturno, R.drawable.sol,
             R.drawable.terra, R.drawable.urano, R.drawable.venus};
 
     @Override
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return imagemPlanetas.length;
         }
 
         @Override
@@ -49,7 +52,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+            //Declarar os componentes do modelo que serão utilizados na lista
+            ImageView imgPlanetas;
+            TextView txtPlanetas;
+
+
+            View view1 = getLayoutInflater().inflate(R.layout.modelo_planetas, null);
+
+            txtPlanetas = view1.findViewById(R.id.txtModeloPlaneta);
+            imgPlanetas = view1.findViewById(R.id.imgModeloPlaneta);
+
+            //Implementar os metodos nos componentes
+            txtPlanetas.setText(nomePlanetas[position]);
+            imgPlanetas.setImageResource(imagemPlanetas[position]);
+            return view1;
         }
     }
 }
